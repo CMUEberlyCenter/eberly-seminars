d9be1a4c43fef0587e24ad7395027c043e90b2e6
Meg Richards <mouse@cmu.edu>
2012-04-03 14:03:10 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GraduateStudents::Application.config.secret_token = '9b250c88266f117f83acdb237f0959c0224202e9a6c231e938578e5cd58ea1103fde020b97d3417b24eba7f06ce8df809994d04ea64c142a4980023400e74715'
