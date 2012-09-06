b8314bbe08252d768e6e3b33affe917a9a39b363
Meg Richards <mouse@cmu.edu>
2012-09-06 11:47:20 -0400

    Revert "Removed script tags."
    This reverts commit 584342a87d80af2ceb2d1385536e6ce503fc9ae1.

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,3 +1,5 @@
+<script type="text/javascript">
+
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-34621158-1']);
 _gaq.push(['_trackPageview']);
@@ -7,3 +9,5 @@ _gaq.push(['_trackPageview']);
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+</script>
