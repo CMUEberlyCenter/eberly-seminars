272f0d7e6e5abe0b95f42c56c15c10b2d0e96b38
Meg Richards <mouse@cmu.edu>
2014-04-28 19:53:33 -0400

    Update DB seed file to be much less dangerous  * Remove all remaining destroy/create sequences (changes id & breaks fk's)  * Remove all unnecessary seedings (no more garbage data)  * All remaining seeded information (e.g. application settings) now     done as a find_or_create to prevent modifying keys that already exist     and may have been updated by a user Fix always-broken population if initial admins by creating without   protection (mass-assignment of andrewid & is_admin)... would only   happen during seed; no need to be more elegant

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,43 +21,18 @@
 ['attended','attended-incomplete','absent-unexcused','absent-excused'].
   each { |status| AttendanceStatus.find_or_create_by_key( :key => status ) }
 
+# Create certain admins by default if they don't already exist
+['meribyte','jmbrooks','hilaryf','rpoprosk'].
+  each { |andrewid| Participant.find_or_create_by_andrewid( { :andrewid => andrewid, :is_admin => true },
+                                                            :without_protection => true) }
 
+# Create default application-wide settings if they don't already exist
+Setting.find_or_create_by_key(
+                              :key => "default-tag", 
+                              :label => "Default tag for new seminars", 
+                              :value => "S13" )
 
-Participant.destroy_all
-['meribyte','mg2e','jmbrooks','sa0n','hilaryf','lovett'].each { |andrewid| Participant.create(:andrewid => andrewid, :is_admin => 1) }
-['rpoprosk','adrake','lsudol'].each { |andrewid| Participant.create(:andrewid => andrewid, :is_admin => 0) }
-
-Setting.create( :label => "Default tag for new seminars", :key => "default-tag", :value => "S13" )
-Setting.create( :label => "List seminars in admin view with tag", :key => "admin-list-tag", :value => "S13" )
-
-#User.create(:login => 'admin', :role => Role.find_by_name('admin'))
-
-Seminar.destroy_all
-Seminar.create(:title => "Incorporating Writing in Your Discipline",
-               :description => "Writing can facilitate students' learning in any discipline. Consider rationale for writing across the curriculum, discuss low-stakes writing assignments appropriate for a range of courses, and analyze assignments from various disciplines. \(If possible, attend \"Responding to Student Writing\" as well.\)",
-               :location => "CYH A72",
-               :start_at => "2012-05-09 07:04:54 EDT",
-               :end_at => "2012-05-09 12:04:54 EDT",
-               :seminar_status => SeminarStatus.find_by_status('published'),
-               :maximum_capacity => 25)
-
-Seminar.create(:title => "Responding to Student Writing ",
-               :description => "Writing can facilitate students' learning in any discipline. Consider rationale for writing across the curriculum, discuss low-stakes writing assignments appropriate for a range of courses, and analyze assignments from various disciplines. \(If possible, attend \"Responding to Student Writing\" as well.\)",
-               :location => "CYH A72",
-               :start_at => "2012-05-08 07:04:54 EDT",
-               :end_at => "2012-05-08 12:04:54 EDT",
-               :seminar_status => SeminarStatus.find_by_status('published'),
-               :maximum_capacity => 25)
-
-Registration.create(:participant => Participant.find_by_andrewid('lsudol'),
-                    :seminar => Seminar.find_by_title('Incorporating Writing in Your Discipline'),
-                    :registration_status => RegistrationStatus.find_by_status('pending')
-                    )
-
-Registration.create(:participant=> Participant.find_by_andrewid('adrake'),
-                    :seminar => Seminar.find_by_title('Incorporating Writing in Your Discipline'),:registration_status => RegistrationStatus.find_by_status('pending'))
-Registration.create(:participant => Participant.find_by_andrewid('jmbrooks'),
-                    :seminar => Seminar.find_by_title('Incorporating Writing in Your Discipline'),:registration_status => RegistrationStatus.find_by_status('pending'))
-               
-                                                       
-
+Setting.find_or_create_by_key( 
+                              :key => "admin-list-tag", 
+                              :label => "List seminars in admin view with tag", 
+                              :value => "S13" )
