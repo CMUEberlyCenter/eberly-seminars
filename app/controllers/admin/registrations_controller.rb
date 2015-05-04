d2d794acc75303cebeb0e1d489aa4236a3811604
Meg Richards <mouse@cmu.edu>
2015-05-04 11:10:10 -0400

    Fix manual enrollment to use new params style.

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -8,7 +8,7 @@ class Admin::RegistrationsController < ApplicationController
   end
 
   def create  
-    @registration = Registration.new(params[:registration] )
+    @registration = Registration.new( registration_params )
     @registration.seminar_id = params[:seminar_id]
     @registration.attendance_status = AttendanceStatus.find_by_key('attended')
     
@@ -28,5 +28,11 @@ class Admin::RegistrationsController < ApplicationController
     redirect_to(admin_seminar_path(params[:seminar_id]), :notice => 'Invalid AndrewID')
     #render :controller => 'admin/seminars', :action =>"new" ,:notice => 'Invalid AndrewID'
   end 
-  
+
+  private
+  def registration_params
+    if current_user.is_admin?
+      params.require(:registration).permit(:id, :seminar, :seminar_id, :participant_id, :registration_status_id, :attendance_status_id, :search_string)
+    end
+  end
 end
