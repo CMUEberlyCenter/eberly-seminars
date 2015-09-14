b8bae4ccbceee859f8b3c3e56d954b1f3653d4d5
Meg Richards <mouse@cmu.edu>
2015-09-14 14:59:22 -0400

    Replace assignment notification panel with nudge notification panel in admin settings

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -5,7 +5,7 @@ class Admin::SettingsController < ApplicationController
     @settings = Setting.all
     @consultants = Participant.where(consultant: true)
     @pending_graduates = Programs::FutureFaculty::RequirementsVersion.last.pending_graduates
-    @unassigned_activities =  Participant::Activity.all.where("future_faculty_requirement_id > 6").where("observer_id is null")
+    @nudges = Participant::Activity.all.where("type='Participant::Activities::MicroteachingWorkshop' or type='Participant::Activities::EarlyCourseFeedback' or type='Participant::Activities::TeachingObservation'").where("memo_completed_on is null").where("completed_on < ?",3.weeks.ago)
   end
 
   def update
