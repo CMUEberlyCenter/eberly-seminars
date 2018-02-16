7d84fff5b9bf46768284849b22c463b191dfbaa1
Martin van Velsen <vvelsen@gmail.com>
2018-02-16 11:54:05 -0500

    Added a small check in the seminars controller for those instances where someone connects to the system without having anything configured.

--- app/controllers/seminars_controller.rb
+++ app/controllers/seminars_controller.rb
@@ -1,14 +1,31 @@
 class SeminarsController < ApplicationController
   skip_before_filter :require_authentication, only: :index
 
+  private def check_install
+    print ("Checking installation ...\n")
+
+    # Listing of all tables and views
+    ActiveRecord::Base.connection.tables
+
+    # Checks for existence of seminars table/view
+    return (ActiveRecord::Base.connection.table_exists? 'seminars')
+  end
+
   def index
-    if administrator?
-      flash.keep
-      redirect_to admin_seminars_url
-     else
-       #flash[:notice] = "Announcement text"
-       @seminars = Seminar.active.published
-    end
+    if (check_install == false)
+      render html: "This appears to be a new installation, please make sure you have configured the datbase properly. Run 'rake db:migrate' or 'rake:load' first."
+    else
+      if administrator?
+        flash.keep
+        redirect_to admin_seminars_url
+       else
+        #flash[:notice] = "Announcement text"
+        @seminars = Seminar.active.published
+        if !@seminars
+          render html: "This appears to be a new installation, please make sure you have basic data in your database. Run 'rake db:seed' first."
+        end  
+      end
+    end   
   end
 
 end
