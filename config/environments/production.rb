46e48203dcc006424b0c4499055f7aefa835bcf3
Meg Richards <mouse@cmu.edu>
2018-10-11 16:20:20 -0400

    Re-Add SMTP Configuration
    Never got put in with R5 upgrade.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mail configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "relay.andrew.cmu.edu",
+    :domain  => "cmu.edu",
+    :port    => 25
+  }
 end
